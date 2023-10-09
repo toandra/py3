988e5b5372e4aaa4abece42199ccb2af1c6fc77c
toandra <aandrade0205@icloud.com>
2023-10-08 20:44:30 -0500

    cleaning up a little

--- running-sum.py
+++ running-sum.py
@@ -1,16 +1,19 @@
-from ast import List
-
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum(self, nums: list[int]) -> list[int]:
         results = []
         results.append(nums[0])
 
         index = 1
         end = len(nums)
 
+        # input list example: (1,2,3,4)
+      
         for num in nums:
             if index < end:
                 results.append( nums[index] + results[index - 1] )
                 index += 1
 
+        # output result should be: (1,3,6,10)
         return results
+    
+print(Solution().runningSum([1,2,3,4]))
