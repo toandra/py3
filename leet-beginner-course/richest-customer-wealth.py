3b2a98819e28967719311c47c5f9e429d9fc5f4f
toandra <aandrade0205@icloud.com>
2023-10-09 21:55:00 -0500

    cleaning up

--- leet-beginner-course/richest-customer-wealth.py
+++ leet-beginner-course/richest-customer-wealth.py
@@ -6,17 +6,13 @@ class Solution:
         
         for account_index, account in enumerate(accounts): 
             total = 0
+
             for wealth_index, wealth in enumerate(account): 
-                total += accounts[account_index][wealth_index]  
+                total += accounts[account_index][wealth_index]
+                  
             results.append(total)
 
-        for result in results: 
-            if largest == 0: 
-                largest = result
-            if largest < result:
-                largest = result
-
-        return largest
+        return max(results)
 
 
 print(Solution().maximumWealth([[1,2,3],[12,1,22],[2,2,2]]))
