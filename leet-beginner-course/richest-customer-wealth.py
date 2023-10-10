d7574c0eff05bf6adfd4a6bd61f354ef205658c3
toandra <aandarde0205@icloud.com>
2023-10-09 22:00:49 -0500

    not using max method

--- leet-beginner-course/richest-customer-wealth.py
+++ leet-beginner-course/richest-customer-wealth.py
@@ -6,13 +6,17 @@ class Solution:
         
         for account_index, account in enumerate(accounts): 
             total = 0
-
             for wealth_index, wealth in enumerate(account): 
-                total += accounts[account_index][wealth_index]
-                  
+                total += accounts[account_index][wealth_index]  
             results.append(total)
 
-        return max(results)
+        for result in results: 
+            if largest == 0: 
+                largest = result
+            if largest < result:
+                largest = result
+
+        return largest
 
 
 print(Solution().maximumWealth([[1,2,3],[12,1,22],[2,2,2]]))
