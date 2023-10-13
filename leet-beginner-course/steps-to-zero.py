a6a86fcd36b2890a081ba9f5570eb4a13740ac8b
toandra <aandrade0205@icloud.com>
2023-10-12 20:02:18 -0500

    Count steps to get to zero

---
+++ leet-beginner-course/steps-to-zero.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+
+        steps = 0
+        
+        while num != 0: 
+            steps += 1
+
+            if num % 2 == 0: 
+                num = num / 2
+            else:
+                num = num - 1 
+        
+        return steps
+    
